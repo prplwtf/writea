71647c08fced86b6f053eb551b2ac8bc34c639dc
prplwtf <hello@prpl.wtf>
2024-05-26 00:16:25 +0200

    feat `NavigationBarElement`: make link configurable

--- src/components/elements/NavigationBarElement.js
+++ src/components/elements/NavigationBarElement.js
@@ -3,6 +3,16 @@ function NavigationBarElement() {
   if(window.location.hash != "#blog") {
     ButtonClass = "btn-danger"
   }
+
+  let LinkElement = ""
+  if(window.Configuration.Application.Link) {
+    LinkElement = `
+      <i
+        class="bi bi-link-45deg text-danger-emphasis float-end me-3" style="font-size:25px; cursor:pointer"
+        onclick="window.location = '${window.Configuration.Application.Link}'"
+      ></i>
+    `
+  }
   
   return `
     <div class="row mb-3">
@@ -15,10 +25,7 @@ function NavigationBarElement() {
         <button type="button" class="btn ${ButtonClass} float-end rounded-5" onclick="Route('#blog')">
           posts
         </button>
-        <i
-          class="bi bi-link-45deg text-danger-emphasis float-end me-3" style="font-size:25px; cursor:pointer"
-          onclick="window.location = 'https://prpl.wtf'"
-        ></i>
+        ${LinkElement}
       </div>
     </div>
   `
--- src/configuration/RenderConfiguration.js
+++ src/configuration/RenderConfiguration.js
@@ -1,6 +1,5 @@
 async function RenderConfiguration() {
   // Render values
   AppTitle.innerHTML = window.Configuration.Application.Title
-
   return;
 }
